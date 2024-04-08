c7d9d3c09d0154ffcf3284faf7b0a453a01271de
Jonathan Berglind <jonathan@evam.life>
2024-04-08 16:34:45 +0200

    Update copy

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ export const HomePage = ({ onClickButton }: { onClickButton: () => void }) => {
   return (
     <Stack alignItems={"center"} gap={"P4"} pt={"P6"}>
       <Heading>Welcome</Heading>
-      <Text>Welcome to your very own certified app!</Text>
+      <Text>This is your very own certified app!</Text>
       <Button variant={"primary"} onClick={onClickButton}>
         Explore SDK
       </Button>
